2249372eda58af6068f302bb26ba2563eff71cce
Lalit Thakre <lalit03thakre@gmail.com>
2023-04-10 12:48:16 +0530

    commit

--- lib/screens2/home_screen.dart
+++ lib/screens2/home_screen.dart
@@ -90,7 +90,7 @@ class _HomeScreen1State extends State<HomeScreen1> {
                   style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
                 ),
                 CircleAvatar(
-                  radius: 28,
+                  radius: 38,
                   backgroundImage: NetworkImage(userMap?['url'] != null
                       ? "${userMap?['url']}"
                       : "https://imgs.search.brave.com/05TBeNcAKK_r3R0LB3pKtpxtWDXWh8ivakrk0aYd5_I/rs:fit:322:294:1/g:ce/aHR0cHM6Ly9zdGVl/bWl0aW1hZ2VzLmNv/bS9EUW1XQW9lVXBR/RFRaaUNoSjUxTFRG/U0NBMndWcUEybWpZ/WlVUWE5teldVS1pO/Qi9kb2N1Ym90Lmdp/Zg.gif"),
@@ -345,7 +345,9 @@ class _HomeScreen1State extends State<HomeScreen1> {
           //     );
           //   },
           // ),
-          Image.asset("images/botgif.jpg"),
+          Center(
+              child: Container(
+                  height: 190, child: Image.asset("images/botgif.jpg"))),
           Center(
             child: Text(
               "Your AI chatbot is here !",
